84c113cb4e05f47ba8abd802a7f116d302006c44
Ryan Edge <ryan@superformula.com>
2021-01-19 00:33:56 -0500

    feat: create initial project

---
+++ lib/package_releases.dart
@@ -0,0 +1,12 @@
+library package_releases;
+
+class Calculator {
+  /// Returns [value] plus 1.
+  int addOne(int value) => value + 1;
+
+  /// Returns [value] plus 2.
+  int addTwo(int value) => value + 2;
+
+  /// Returns [value] plus 3.
+  int addThree(int value) => value + 3;
+}
