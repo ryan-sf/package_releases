6740eb4313a0adf949690acfa5f49bc2fd846820
Ryan Edge <57107691+ryan-sf@users.noreply.github.com>
2021-01-21 01:16:09 -0500

    fix: break addFour fn

--- lib/package_releases.dart
+++ lib/package_releases.dart
@@ -10,6 +10,6 @@ class Calculator {
   /// Returns [value] plus 3.
   int addThree(int value) => value + 3;
 
-  /// Returns [value] plus 4.
-  int addFour(int value) => value + 4;
+  /// Returns [value] plus 5.
+  int addFour(int value) => value + 5;
 }
